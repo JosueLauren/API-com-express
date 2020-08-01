ef307d89bd7efb8f6b4ef001526df54ec21e0d7e
JosueLauren <josuelauren_@hotmail.com>
2020-08-01 19:59:13 -0300

    feita as operações do banco

--- src/bancoDeDados.js
+++ src/bancoDeDados.js
@@ -1,6 +1,6 @@
 const sequence = {
     _id: 1,
-    get id() { return this.id++}
+    get id() { return this._id++}
 }
 
 const produtos = {}
@@ -20,8 +20,15 @@ function getProdutos() {
     return Object.values(produtos)
 }
 
+function excluirProduto(id) {
+    const produto = produtos[id]
+    delete produtos[id]
+    return produto
+}
+
 module.exports = {
     salvarProduto,
     getProduto,
-    getProdutos
+    getProdutos,
+    excluirProduto
 }
--- src/servidor.js
+++ src/servidor.js
@@ -2,21 +2,40 @@ const  porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) =>{
     res.send(bancoDeDados.getProdutos())
 })
 
-app.get('/produto/:id', (req, res, next) =>{
+app.get('/produtos/:id', (req, res, next) =>{
     res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.post('/produto', (req, res, next) =>{
+app.post('/produtos', (req, res, next) =>{
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
+
+    res.send(produto) // JSON
+})
+
+app.put('/produtos/:id', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) =>{
+    const produto = bancoDeDados.excluirProduto(req.params.id)
 
     res.send(produto) // JSON
 })
